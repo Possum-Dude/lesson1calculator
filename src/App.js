f8261f6332524babc3aa39e24d40f23b4702cffb
Possum Dude <possumdude1@gmail.com>
2023-06-19 14:47:45 +1200

    added hook constants and calculation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from'react';
 
-function App() {
+
+const App = (props) => {
+
+
+const[ x, setX ] = useState(0);
+const[ answer, setAnswer ] = useState("");
+const[ first, setFirst] = useState(10);
+const[ second, setSecond] = useState(22);
+
+const Calculate = (value) => {
+  console.log("what is value?", value);
+
+  if (value !== "") {
+
+switch (value) {
+
+case "-": 
+setAnswer(first - second);
+break;
+case"+":  setAnswer(first + second);
+break;
+case "/":  setAnswer(first / second);
+break;
+case "*":  setAnswer(first * second);
+break;
+default:  setAnswer("Error");
+}
+
+
+  }
+}
+
+var Add = (x) =>{
+  x=x+1;
+  console.log(x);
+  setX(x);
+  return x;
+}
+
   return (
     <div className="App">
+
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Boop
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simple Calculator</h1>
+        <input type="number"
+         value={Number(first)}
+          onChange={(e) => setFirst(e.target.value)}/>
+
+          <input type="number"
+         value={Number(second)}
+          onChange={(e) => setSecond(e.target.value)}/>  
+
+          <input type="number" defaultValue={answer}/> 
+         <div><button className='Addbutton' onClick={() =>Calculate("+")}>+</button>
+         <button className='Addbutton' onClick={() =>Calculate("-")}>-</button>
+         <button className='Addbutton' onClick={() =>Calculate("/")}>/</button>
+         <button className='Addbutton' onClick={() =>Calculate("*")}>*</button></div>
+
+        <button className="Addbutton" onClick={() => Add(x)}>
+  Add 1 to x
+</button>
       </header>
     </div>
   );
